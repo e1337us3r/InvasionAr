9ed3951b5b049b31480d3c548cf1c277ffe1ec9a
Sinan Sakaoglu <sinanssakaoglu@gmail.com>
2018-09-27 16:27:55 +0300

    * Ability to create AR scene and view. * Detect possible planes. * Tapping on a plane places earth node and removes plane detectors, detected planes.
    Signed-off-by: Sinan Sakaoglu <sinanssakaoglu@gmail.com>

--- app/src/main/java/com/apolets/arinvaders/MainActivity.kt
+++ app/src/main/java/com/apolets/arinvaders/MainActivity.kt
@@ -1,12 +1,243 @@
 package com.apolets.arinvaders
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.apolets.arinvaders.Static.Configuration
+import com.viro.core.*
+import com.viro.core.Vector
+import java.io.IOException
+import java.io.InputStream
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var viroView: ViroView
+    private lateinit var arScene: ARScene
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        viroView = ViroViewARCore(this, object : ViroViewARCore.StartupListener {
+            override fun onSuccess() {
+                displayScene()
+            }
+
+            override fun onFailure(error: ViroViewARCore.StartupError?, errorMessage: String?) {
+                Log.d(Configuration.DEBUG_TAG, "Error initializing AR: $errorMessage")
+            }
+
+        })
+
+
+        //Use this view as the main view instead of an xml layout file.
+        setContentView(viroView)
+    }
+
+
+    private fun displayScene() {
+
+        // Create the 3D AR scene, and display the point cloud
+        arScene = ARScene()
+        arScene.displayPointCloud(true)
+
+
+        // Create a TrackedPlanesController to visually display identified planes .
+        val controller = PlanesController()
+
+        // Spawn a 3D Droid on the position where the user has clicked on a tracked plane.
+        controller.addOnPlaneClickListener(object : ClickListener {
+            override fun onClick(i: Int, node: Node, clickPosition: Vector) {
+                Log.d(Configuration.DEBUG_TAG, "On plane click")
+                spawnEarth(clickPosition)
+
+                //Remove plane detection and click listener
+                controller.removeOnPlaneClickListener(this)
+                arScene.setListener(null)
+                arScene.displayPointCloud(false)
+            }
+
+            override fun onClickState(i: Int, node: Node, clickState: ClickState, vector: Vector) {
+                //No-op
+            }
+        })
+
+        arScene.setListener(controller)
+
+        viroView.scene = arScene
+
+
+    }
+
+    private fun spawnEarth(position: Vector) {
+        // Create a droid on the surface
+        val bot = getBitmapFromAsset(this, "earth_ball.jpg")
+        val object3D = Object3D()
+        object3D.setPosition(position)
+
+        arScene.rootNode.addChildNode(object3D)
+
+        // Load the Android model asynchronously.
+        object3D.loadModel(viroView.viroContext, Uri.parse("file:///android_asset/earth_ball.obj"), Object3D.Type.OBJ, object : AsyncObject3DListener {
+            override fun onObject3DLoaded(obj: Object3D, type: Object3D.Type) {
+                // When the model is loaded, set the texture associated with this OBJ
+                val objectTexture = Texture(bot, Texture.Format.RGBA8, false, false)
+                val material = Material()
+
+                material.diffuseTexture = objectTexture
+
+                obj.geometry.materials = Arrays.asList(material)
+
+                //Model has to be scaled down programmatically since it is too big
+                obj.setScale(Vector(0.001, 0.001, 0.001))
+                Log.d(Configuration.DEBUG_TAG, "Model loaded")
+            }
+
+            override fun onObject3DFailed(s: String) {
+                Log.d(Configuration.DEBUG_TAG, "Model load failed :$s")
+            }
+        })
+
+        Log.d(Configuration.DEBUG_TAG, "coordinates: x:${object3D.positionRealtime.x}, y:${object3D.positionRealtime.y}, z:${object3D.positionRealtime.z} root: ${arScene.rootNode.positionRealtime}")
+
+    }
+
+    private fun getBitmapFromAsset(context: Context, assetName: String): Bitmap? {
+        val assetManager = context.resources.assets
+        val imageStream: InputStream
+        try {
+            imageStream = assetManager.open(assetName)
+        } catch (exception: IOException) {
+            Log.d(Configuration.DEBUG_TAG, "Unable to find image [" + assetName + "] in assets! Error: "
+                    + exception.message)
+            return null
+        }
+
+        return BitmapFactory.decodeStream(imageStream)
+    }
+
+    private inner class PlanesController : ARScene.Listener {
+
+
+        private val surfaces = HashMap<String, Node>()
+        private val planeClickListeners = HashSet<ClickListener>()
+
+
+        fun addOnPlaneClickListener(listener: ClickListener) {
+            planeClickListeners.add(listener)
+        }
+
+        fun removeOnPlaneClickListener(listener: ClickListener) {
+            if (planeClickListeners.contains(listener)) {
+                planeClickListeners.remove(listener)
+            }
+        }
+
+        override fun onTrackingInitialized() {
+
+        }
+
+        override fun onTrackingUpdated(p0: ARScene.TrackingState?, p1: ARScene.TrackingStateReason?) {
+        }
+
+        override fun onAmbientLightUpdate(p0: Float, p1: Vector?) {
+        }
+
+        override fun onAnchorUpdated(arAnchor: ARAnchor?, p1: ARNode?) {
+            if (arAnchor?.type == ARAnchor.Type.PLANE) {
+                val planeAnchor = arAnchor as ARPlaneAnchor
+
+                // Update the mesh surface geometry
+                val node = surfaces[arAnchor.anchorId]
+                val plane = node!!.geometry as Surface
+                val dimensions = planeAnchor.extent
+                plane.width = dimensions.x
+                plane.height = dimensions.z
+            }
+        }
+
+        override fun onAnchorFound(arAnchor: ARAnchor?, arNode: ARNode?) {
+
+            // Spawn a visual plane if a PlaneAnchor was found
+            if (arAnchor?.type == ARAnchor.Type.PLANE) {
+                val planeAnchor = arAnchor as ARPlaneAnchor
+
+                // Create the visual geometry representing this plane
+                val dimensions = planeAnchor.extent
+                val plane = Surface(1f, 1f)
+                plane.width = dimensions.x
+                plane.height = dimensions.z
+
+                // Set a default material for this plane.
+                val material = Material()
+                material.diffuseColor = Color.parseColor("#eff1f4")
+                material.blendMode = Material.BlendMode.ALPHA
+                plane.materials = Arrays.asList(material)
+
+                // Attach it to the node
+                val planeNode = Node()
+                planeNode.geometry = plane
+                planeNode.setRotation(Vector(-Math.toRadians(90.0), 0.0, 0.0))
+                planeNode.setPosition(planeAnchor.center)
+
+                // Attach this planeNode to the anchor's arNode
+                arNode?.addChildNode(planeNode)
+                surfaces[arAnchor.getAnchorId()] = planeNode
+
+                // Attach click listeners to be notified upon a plane onClick.
+                planeNode.clickListener = object : ClickListener {
+                    override fun onClick(i: Int, node: Node, vector: Vector) {
+                        for (listener in planeClickListeners) {
+                            listener.onClick(i, node, vector)
+                            //Remove detected planes from scene
+                            arNode?.removeAllChildNodes()
+                        }
+                    }
+
+                    override fun onClickState(i: Int, node: Node, clickState: ClickState, vector: Vector) {
+                        //No-op
+                    }
+                }
+
+
+            }
+
+        }
+
+        override fun onAnchorRemoved(arAnchor: ARAnchor?, p1: ARNode?) {
+            surfaces.remove(arAnchor?.anchorId)
+        }
+    }
+
+
+    // Lifecycle methods
+    override fun onStart() {
+        super.onStart()
+        viroView.onActivityStarted(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viroView.onActivityResumed(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        viroView.onActivityPaused(this)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        viroView.onActivityStopped(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        viroView.onActivityDestroyed(this)
     }
 }
---
+++ app/src/main/java/com/apolets/arinvaders/Static/Configuration.kt
@@ -0,0 +1,16 @@
+package com.apolets.arinvaders.Static
+
+// global config objects are supposedly a bad pattern...
+// I already moved some stuff out of here; we can probably get
+// rid of this class entirely
+class Configuration {
+
+    companion object {
+
+        const val SECONDS_BETWEEN_WAVES_SHORT = 30
+        const val SECONDS_BETWEEN_WAVES_MEDIUM = 60
+        const val SECONDS_BETWEEN_WAVES_LONG = 90
+
+        const val DEBUG_TAG = "MYAPP"
+    }
+} // end class
